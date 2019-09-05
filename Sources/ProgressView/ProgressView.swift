09e0ff401595402ed8c679306430fbdd96931bf7
ericlewis <ericlewis777@gmail.com>
2019-09-05 17:43:39 -0400

    public init

--- Sources/ProgressView/ProgressView.swift
+++ Sources/ProgressView/ProgressView.swift
@@ -3,6 +3,10 @@ import SwiftUI
 public struct ProgressView: UIViewRepresentable {
     @Binding public var value: Float
     
+    public init(_ value: Binding<Float>) {
+        _value = value
+    }
+    
     public func makeUIView(context: Context) -> UIProgressView {
         UIProgressView()
     }
